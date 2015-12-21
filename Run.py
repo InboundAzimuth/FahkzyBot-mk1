c3ae2229023d5e53979b22ed3b82c233d5224fe6
Robert Toukmond <inboundazimuth@gmail.com>
2015-12-20 23:17:59 -0800

    Uptime!
    Got some uptime! :dash:

--- Run.py
+++ Run.py
@@ -1,13 +1,20 @@
+import parser
+import time
+import datetime
 import string
 from Read import getUser, getMessage
 from Socket import openSocket, sendMessage
 from Init import joinRoom
-import parser
+
 
 s = openSocket()
 joinRoom(s)
 readbuffer = ""
-#components = parser.parse_command(command)
+start_time = time.time()
+
+#I KNOW HOW SHIT THIS BOT IS, BUT HONESTLY. I DON'T CARE :D
+#I WAS BORED WHEN I MADE THIS
+#Copyright please Fahkzy
 
 while True:
 		readbuffer = readbuffer + s.recv(1024)
@@ -21,7 +28,7 @@ while True:
 				sendMessage(s, "I just recieved a ping, and sent back a pong!")
 				break
 			user = getUser(line)
-			message.upper() = getMessage(line)
+			message = getMessage(line).upper()
 			print user + " typed :" + message
 			#You suck and bad
 			if "You suck" in message in message:
@@ -32,13 +39,20 @@ while True:
 				sendMessage(s, "Fahkzy's trade link: https://steamcommunity.com/tradeoffer/new/?partner=65390294&token=efmHK3WH")
 				break
 			#Help Command
-			if "!help" in message:
+			if "!HELP" in message:
 				sendMessage(s, "All commands are case sensitive.")
 				sendMessage(s, "I am a lazy developer.")
 				sendMessage(s, "The current commands:")
 				sendMessage(s, "!help, !trade, ")
 				break
 			#Hai
-			if "Hi" in message:
+			if "HI" in message:
 				sendMessage(s, "Hai I'm a bot!")
+				break
+			#Uptime
+			if "!UPTIME" in message:
+				end_time = time.time()
+				uptime = end_time - start_time
+				uptime = str(datetime.timedelta(seconds=int(uptime)))
+				sendMessage(s,"Uptime: " + uptime)
 				break
